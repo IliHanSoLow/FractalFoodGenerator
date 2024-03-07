4fcbc007ffa91713052ac99fbcb06e87f99802f8
IliHanSoLow <ilian-ode@gmx.de>
2024-03-07 22:26:23 +0100

    setup ocaml

---
+++ bin/main.ml
@@ -0,0 +1,8 @@
+open Tsdl
+
+let const pi = 3.14159265359
+let const max_iters = 16
+let const window_w_init = 700
+let const window_h_init = 700
+let main () = print_endline "Hello, World!"
+let () = main ()
---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (
+      system: let
+        pkgs = import nixpkgs;
+      in
+        with pkgs; {
+          devShells.default = mkShell {
+            buikdInputs = [
+              opam
+              dune_3
+              ocamlPackages.tsdl
+            ];
+          };
+        }
+    );
+}
